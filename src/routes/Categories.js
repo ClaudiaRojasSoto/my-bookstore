115b4ce2cd1df8a66f79b87029b34f6000e4ea20
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-17 12:43:31 -0400

    Create the Ctaegories.js file instead Authors.js file that was deleted

---
+++ src/routes/Categories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Categories = () => (
+  <div>
+    <h2>Authors Page</h2>
+    {/* Contenido de la página de categorías */}
+  </div>
+);
+
+export default Categories;
