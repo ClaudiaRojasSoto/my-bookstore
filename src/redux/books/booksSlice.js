d06e3fc517b7f0877ac3449112af448297110e28
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-17 17:54:03 -0400

    Create an initialState variable which includes 3 elements with item_id, author, category and title instead the empty variable from the initial configuration

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,6 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = [];
+const initialState = [
+  {
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
+];
 
 const booksSlice = createSlice({
   name: 'books',
@@ -9,7 +28,12 @@ const booksSlice = createSlice({
     addBook: (state, action) => {
       state.push(action.payload);
     },
-    removeBook: (state, action) => state.filter((book) => book.id !== action.payload),
+    removeBook: (state, action) => {
+      const index = state.findIndex((book) => book.item_id === action.payload);
+      if (index !== -1) {
+        state.splice(index, 1);
+      }
+    },
   },
 });
 
