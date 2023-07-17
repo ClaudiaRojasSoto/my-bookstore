df547abeac9e7a4bb29d8cb0fac29692821232f3
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-17 17:56:28 -0400

    Display the books received from the slice and dispatch actions

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
+
 import Book from './Book';
 
-const BookList = ({ books, onDelete }) => (
-  <div>
-    {books.map((book) => (
-      <Book key={book.id} book={book} onDelete={onDelete} />
-    ))}
-  </div>
-);
+const BookList = () => {
+  const dispatch = useDispatch();
+  const books = useSelector((state) => state.books);
 
-BookList.propTypes = {
-  books: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
-  onDelete: PropTypes.func.isRequired,
+  const handleDelete = (bookId) => {
+    dispatch(removeBook(bookId));
+  };
+
+  return (
+    <div>
+      {books.map((book) => (
+        <Book key={book.item_id} book={book} onDelete={handleDelete} />
+      ))}
+    </div>
+  );
 };
 
 export default BookList;
