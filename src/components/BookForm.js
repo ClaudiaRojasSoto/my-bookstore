c301ff5582c41a4c2b44b40fcf5fa268233217e9
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-17 23:50:03 -0400

    Add the category on the handleSubmit function from the redux state and use the selector to get the category

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux'; // Añade useSelector
 import { addBook } from '../redux/books/booksSlice';
+import { selectCategory } from '../redux/categories/categoriesSlice'; // Asegúrate de tener este selector en tu slice
 
 const BookForm = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const dispatch = useDispatch();
+  const category = useSelector(selectCategory); // Usa el selector para obtener la categoría
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const newBook = {
       title,
       author,
+      category, // Usa la categoría desde el estado de Redux
       item_id: Date.now().toString(),
     };
     dispatch(addBook(newBook));
