981e5ba92dd283f6112fab5ba8c8145e414a04bb
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-19 17:24:26 -0400

    Add the elements without functionality to follow the requirements and import the circular progress bar

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import { removeBook } from '../redux/books/booksSlice';
 
 const Book = ({ book }) => {
@@ -17,10 +19,39 @@ const Book = ({ book }) => {
         <p className="author">
           {book.author}
         </p>
-        <button className="button" type="button" onClick={handleDelete}>
-          Delete
-        </button>
+        <div className="buttons">
+          <button className="button" type="button">
+            Comments
+          </button>
+          <button className="button" type="button" onClick={handleDelete}>
+            Remove
+          </button>
+          <button className="button" type="button">
+            Edit
+          </button>
+        </div>
       </div>
+      <div>
+        <div className="circle">
+          <CircularProgressbar />
+        </div>
+        <div>
+          <p>0%</p>
+          <p>Completed</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <p>CURRENT CHAPTER</p>
+          <p>Chapter 1</p>
+        </div>
+        <div>
+          <button className="button-update" type="button">
+            UPDATE PROGRESS
+          </button>
+        </div>
+      </div>
+
     </div>
   );
 };
